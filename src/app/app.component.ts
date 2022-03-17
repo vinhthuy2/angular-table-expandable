11c09f53c062597b3c8c127c6c0454bd5c873684
TRAN VINH THUY <mr.thuytran@gmail.com>
2022-03-18 00:02:26 +0700

    add expansion to table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'expandable-table';
+
+  actions = [
+    'Confirm',
+    'Manual',
+    'Auto',
+  ]
+  data = [
+    {
+      date: 'dd-mm-yyyy',
+      name: 'Tran Vinh Thuy',
+      action: 1,
+    },
+    {
+      date: 'dd-mm-yyyy',
+      name: 'Tran Vinh Thuy',
+      action: 2,
+      expanded: false
+    },
+    {
+      date: 'dd-mm-yyyy',
+      name: 'Tran Vinh Thuy',
+      action: 1
+    },
+    {
+      date: 'dd-mm-yyyy',
+      name: 'Tran Vinh Thuy',
+      action: 2,
+      expanded: false
+    },
+    {
+      date: 'dd-mm-yyyy',
+      name: 'Tran Vinh Thuy',
+      action: 3
+    },
+  ]
+
+  toggleExpansion(item: { date: string, name: string, action: number, expanded?: boolean }) {
+    item.expanded = !item.expanded
+  }
+
+
 }
